5775db2c4b9335557915ef31b2d1558718b63122
Kyle Grealis <84798289+kyleGrealis@users.noreply.github.com>
2025-02-06 14:37:13 -0600

    Update debian-post-install-2.sh

--- debian-post-install-2.sh
+++ debian-post-install-2.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+# Verify that the NVIDIA drivers were correctly installed and nouveau kernel has been replaced
+
 nvidia-smi
 lspci -nnk | grep -A3 NVIDIA
 lsmod | grep nvidia
